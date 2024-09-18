ac9a9e44f028dae8c06d163500e34de1272bfd3f
Serhat Sağlam <serhatsaglamb@gmail.com>
2024-09-18 23:23:52 +0300

    Required files created

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,13 +11,12 @@ using System.Windows.Shapes;
 
 namespace WPF_UserDataBase;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
     }
+
+    
 }
---
+++ classes/usrData.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace WPF_UserDataBase;
+
+public class usrData{
+    public usrData(){
+
+    }
+
+    private string Name {get; set;}
+    private string Surname {get; set;}
+    private int Age {get; set;}
+}
